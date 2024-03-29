8ff8e805516a3b6a00852a228934c21e88a3e595
KirilD77 <kirildalek111@gmail.com>
2024-03-29 17:35:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express, { json } from 'express'
+import cors from 'cors'
 const app = express()
 const port = 3000
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 app.use(json())
 
 app.post('/', (req, res) => {
   console.log(req.body)
-  res.send(JSON.stringify(req.body))
+
+  res.setHeader('Content-Type', 'application/json')
+  res.send(req.body)
 })
 
 app.listen(port, () => {
